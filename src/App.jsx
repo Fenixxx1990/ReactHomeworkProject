16116859dc382c27fe8073fa7352deae15739376
Fenix <fenixxx1990@yandex.ru>
2026-03-11 21:25:43 +0400

    1-start

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import Button from "./Components/Button/Button";
+import Header from "./Components/Header/Header";
+import Paragraph from "./Components/Paragraph/Paragraph";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header title="Поиск" />
+      <Paragraph text="Введите название фильма, сериала или мультфильма для поиска и добавления в избранное." />
+      <Button />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+export default function Header({ title }) {
+  return (
+    <>
+      <h1 className="header">{title}</h1>
+    </>
+  );
+}
